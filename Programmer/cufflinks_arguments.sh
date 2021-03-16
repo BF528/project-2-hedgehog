d2bbae5d7bcf9d2db12eadd829e9dfa360fcc82e
sktpndtbf528 <78941309+sktpndtbf528@users.noreply.github.com>
2021-03-16 15:20:07 -0400

    Update cufflinks_arguments.sh

--- Programmer/cufflinks_arguments.sh
+++ Programmer/cufflinks_arguments.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-cufflinks --compatible-hits-norm -G < reference GTF path on SCC> -u -o P0_1_cufflinks -p 16
+cufflinks --compatible-hits-norm -G <path_to_ref_GTF> -u -o P0_1_cufflinks -p 16
