ecf3dc8e07fdd7b6e620adbd2952c81ac592a553
sktpndtbf528 <78941309+sktpndtbf528@users.noreply.github.com>
2021-03-16 15:20:32 -0400

    Update tophat_arguments.sh

--- Programmer/tophat_arguments.sh
+++ Programmer/tophat_arguments.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # Paired-end sequencing alignment
-tophat -r 200 -G <path_to_GTF_seq> --segment-length=20 --segment-mismatches=1 --no-novel-juncs -o P0_1_tophat -p 16 <path_to_ref_seq> <path_to_fastq1> <path_to_fastq2>
+tophat -r 200 -G <path_to_ref_GTF> --segment-length=20 --segment-mismatches=1 --no-novel-juncs -o P0_1_tophat -p 16 <path_to_ref_seq> <path_to_fastq1> <path_to_fastq2>
